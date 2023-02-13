f96686071a81fd473505172d6001895b0d6f0d91
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-02-13 22:05:12 +0100

    completing models and start with queries

--- accounts/models.py
+++ accounts/models.py
@@ -10,3 +10,36 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.name
+
+class Products(models.Model):
+    CATEGORY=(
+        ('Indoor','Indoor'),
+        ('Out Door','Out Door')
+    )
+    name = models.CharField(max_length=100,null=True)
+    price=models.FloatField(null=True)
+    category=models.CharField(max_length=200,null=True,choices=CATEGORY)
+    description=models.CharField(max_length=200,null=True,blank=True)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+    tag=models.ManyToManyField(Tag)
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS=(
+        ('Pending','Pending'),
+        ('Out for delivery','Out for delivery'),
+        ('Delivred','Delivred')
+    )
+    customer=models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
+    product=models.ForeignKey(Products,null=True,on_delete=models.SET_NULL)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+    status=models.CharField(max_length=200,null=True,choices=STATUS)
+    
